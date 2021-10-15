e3e65400b466bd22015cb870a292f9cb8903c2f9
Yasuyuki Kikuchi <kikuchi@cloudpack.jp>
2021-10-16 00:01:26 +0900

    fret: 必要なviewを登録

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -3,4 +3,34 @@ from flask_blog import app
 
 @app.route("/")
 def show_entries():
-    return "hello world!"
+    return "全ての記事を表示"
+
+
+@app.route("/entries", methods=["POST"])
+def add_entry():
+    return "新しい記事が登録されました"
+
+
+@app.route("/entries/new", methods=["GET"])
+def new_entry():
+    return "記事の入力フォームを表示"
+
+
+@app.route("/entries/<int:id>", methods=["GET"])
+def show_entry(id):
+    return f"記事{id}を表示"
+
+
+@app.route("/entries/<int:id>/edit", methods=["GET"])
+def edit_entry(id):
+    return f"記事{id}の編集フォームを表示"
+
+
+@app.route("/entries/<int:id>/update", methods=["POST"])
+def update_entry(id):
+    return f"記事{id}が更新されました"
+
+
+@app.route("/entries/<int:id>/delete", methods=["POST"])
+def delete_entry(id):
+    return f"記事{id}が削除されました"
