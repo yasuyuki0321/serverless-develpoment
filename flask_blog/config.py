a66de6a7cd04b29331f41ee6ea599c4e74a9322e
Yasuyuki Kikuchi <kikuchi@cloudpack.jp>
2021-10-17 16:45:15 +0900

    fiet: データをdynamodbに登録するように修正

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,1 +1,5 @@
 DEBUG = True
+DYNAMODB_REGION = "ap-northeast-1"
+AWS_ACCESS_KEY_ID = "abc123"
+AWS_SECRET_ACCESS_KEY = "abc123"
+DYNAMODB_ENDPOINT_URL = "http://web-dynamo-1:8000"
--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,57 +1,54 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from flask_blog.models.entries import Entry
 from datetime import datetime
 
 
 @app.route("/")
 def show_entries():
-    entries = [
-        {"id": 1, "title": "初めての投稿", "text": "初めての内容", "created_at": datetime.now()},
-        {"id": 2, "title": "2つめの投稿", "text": "2つめの内容", "created_at": datetime.now()},
-    ]
-
+    entries = Entry.scan()
+    entries = sorted(entries, key=lambda x: x.id, reverse=True)
     return render_template("entries/index.html", entries=entries)
 
 
-@app.route("/entries", methods=["POST"])
+@app.route('/entries', methods=['POST'])
 def add_entry():
-    return "新しい記事が登録されました"
-
+    entry = Entry(
+        id=int(datetime.now().timestamp()),
+        title=request.form["title"],
+        text=request.form["text"],
+    )
+    entry.save()
+    return redirect(url_for("show_entries"))
 
 @app.route("/entries/new", methods=["GET"])
 def new_entry():
     return render_template("entries/new.html")
-    # return "記事の入力フォームを表示"
 
 
 @app.route("/entries/<int:id>", methods=["GET"])
 def show_entry(id):
-    entry = {"id": 1, "title": "初めての投稿", "text": "初めての内容", "created_at": datetime.now()}
-
+    entry = Entry.get(id)
     return render_template("entries/show.html", entry=entry)
 
 
 @app.route("/entries/<int:id>/edit", methods=["GET"])
 def edit_entry(id):
-    entries = [
-        {"id": 1, "title": "初めての投稿", "text": "初めての内容", "created_at": datetime.now()},
-        {"id": 2, "title": "2つめの投稿", "text": "2つめの内容", "created_at": datetime.now()},
-    ]
-
-    entry = None
-    for e in entries:
-        if e["id"] == id:
-            entry = e
-
+    entry = Entry.get(id)
     return render_template("entries/edit.html", entry=entry)
-    # return f"記事{id}の編集フォームを表示"
 
 
 @app.route("/entries/<int:id>/update", methods=["POST"])
 def update_entry(id):
-    return f"記事{id}が更新されました"
+    entry = Entry.get(id)
+    entry.title = request.form["title"]
+    entry.text = request.form["text"]
+    entry.save()
+    return redirect(url_for("show_entries"))
 
 
 @app.route("/entries/<int:id>/delete", methods=["POST"])
 def delete_entry(id):
-    return f"記事{id}が削除されました"
+    entry = Entry.get(id)
+    entry.delete()
+    return redirect(url_for("show_entries"))
